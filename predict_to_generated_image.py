d42b6cd9ff5b73cd919b3a40c9f4deec143c3e48
Beyazıt UYSAL <66223190+masanbasa3k@users.noreply.github.com>
2023-07-12 14:35:22 +0300

    reorganize
    the non-essential library was deleted, some Turkish words were translated into English

--- predict_to_generated_image.py
+++ predict_to_generated_image.py
@@ -1,23 +1,17 @@
 from keras.models import load_model
-from keras.preprocessing import image
 import numpy as np
 import pickle
 
 model = load_model('classifier_model.h5')
 model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
-
 with open("generated_image.pickle", "rb") as file:
     img = pickle.load(file)
 
-#predict the result
 result = model.predict(img)
 
-# print("Üretilen görüntü rakamı:", result)
+max_high_index = np.argmax(result)
 
-en_yuksek_indeks = np.argmax(result)
+predicted_digit = max_high_index
 
-# Tahmin edilen rakam
-tahmin_edilen_rakam = en_yuksek_indeks
-
-print("Tahmin edilen rakam:", tahmin_edilen_rakam)
+print(f"Predicted digit: {predicted_digit}")
