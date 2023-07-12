aa99bb6eff07e6efb402f5a5db40f1782fcb5190
Beyazıt UYSAL <66223190+masanbasa3k@users.noreply.github.com>
2023-07-12 16:06:02 +0300

    reorganize and optimization
    use a function for model creations in create_classification_model.py

--- create_classification_model.py
+++ create_classification_model.py
@@ -3,44 +3,39 @@ from keras.models import Sequential
 from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from keras.utils import to_categorical
 
-# MNIST veri setini yükle
+def build_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=(28, 28, 1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(units=128, activation='relu'))
+    model.add(Dense(units=10, activation='sigmoid'))
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+# Load MNIST dataset
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# Görüntüleri normalize et ve boyutunu düzenle
+# Normalize and reshape the images
 X_train = X_train.astype('float32') / 255.0
 X_test = X_test.astype('float32') / 255.0
 
-# Etiketleri kategorik hale getir
+# Convert labels to categorical format
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
-# Modeli oluştur
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape = (28, 28, 1), activation =   'relu'))
-model.add(MaxPooling2D(pool_size = (2, 2)))
+# Create the model
+model = build_model()
 
-model.add(Conv2D(32, (3, 3), activation = 'relu'))
-model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Flatten())
-
-model.add(Dense(units = 128, activation = 'relu'))
-model.add(Dense(units = 10, activation = 'sigmoid'))
-
-# model.add(Dense(256, activation='relu'))
-# model.add(Dense(128, activation='relu'))
-# model.add(Dense(64, activation='relu'))
-# model.add(Dense(10, activation='softmax'))
-
-# Modeli derle
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
-# Modeli eğit
+# Train the model
 model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
 
-# Modeli değerlendir
+# Evaluate the model
 test_loss, test_acc = model.evaluate(X_test, y_test)
 print('Test loss:', test_loss)
 print('Test accuracy:', test_acc)
 
-# Modeli kaydet
+# Save the model
 model.save('classifier_model.h5')
